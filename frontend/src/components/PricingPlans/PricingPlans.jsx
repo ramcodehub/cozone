50ff971b36c6f8300dbfc7704030adc6296326f1
Manikanta87 <kmanikanta536@gmail.com>
2025-11-20 16:15:10 +0530

    Responsive

--- frontend/src/components/PricingPlans/PricingPlans.jsx
+++ frontend/src/components/PricingPlans/PricingPlans.jsx
@@ -118,6 +118,7 @@ export default function PricingPlans({ plans }) {
           480: { slidesPerView: 1, spaceBetween: 20 },
           768: { slidesPerView: 2, spaceBetween: 20 },
           992: { slidesPerView: 3, spaceBetween: 20 },
+          1400: { slidesPerView: 4, spaceBetween: 10 },
         }}
       >
         {pricingPlans.map((pricingPlan, idx) => (
