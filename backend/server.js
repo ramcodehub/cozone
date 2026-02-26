5f5ccc4604e0c000fd9f42b5e01763a015838fc9
ramcodehub <ram.dwansys@gmail.com>
2026-02-26 15:32:44 +0530

    Update nodemailer config for production (port 465)

--- backend/server.js
+++ backend/server.js
@@ -60,8 +60,8 @@ let transporter = null;
 if (process.env.EMAIL_USERNAME && process.env.EMAIL_PASSWORD) {
   transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
-    port: 587,
-    secure: false,
+    port: 465,
+    secure: true,
     auth: {
       user: process.env.EMAIL_USERNAME,
       pass: process.env.EMAIL_PASSWORD,
